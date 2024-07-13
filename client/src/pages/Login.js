a9fb423d175037e64cb359553947800aa55af986
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-07-13 12:53:21 +0530

    refactor login page ui

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,33 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Form, Input, Button, message } from "antd";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { FaC } from "react-icons/fa6";
 import axios from "axios";
-import { useDispatch } from "react-redux";
 
 const Login = () => {
-  const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
   const handleSubmit = async (value) => {
+    setIsLoading(true);
     try {
-      debugger;
-      dispatch({
-        type: "SHOW_LOADING",
-      });
       const res = await axios.post(
         `${process.env.REACT_APP_SERVER_URL}/api/users/login`,
         value
       );
-      dispatch({ type: "HIDE_LOADING" });
       if (res.data.message === "Login Fail") {
+        setIsLoading(false);
         return message.error("User Not Found");
       } else {
+        setIsLoading(false);
         message.success("User Logged-In Successfully");
         localStorage.setItem("auth", JSON.stringify(res.data));
         navigate("/");
       }
     } catch (error) {
-      dispatch({ type: "HIDE_LOADING" });
       message.error("Something Went Wrong");
+      setIsLoading(false);
       console.log(error);
     }
   };
@@ -40,32 +38,25 @@ const Login = () => {
     }
   }, [navigate]);
   return (
-    <>
-      <div className="register">
-        <div className="regsiter-form">
-          <h1>SiplHub</h1>
-          <h2>Login Page</h2>
-          <Form layout="vertical" onFinish={handleSubmit}>
-            <Form.Item name="userId" label="User ID">
-              <Input required />
-            </Form.Item>
-            <Form.Item name="password" label="Password">
-              <Input required type="password" />
-            </Form.Item>
-            {/* <p className="test-creds">UserId: 123 | Password: 123</p> */}
-            <div className="d-flex justify-content-between">
-              {/* <p>
-                not a user? Please
-                <Link to="/register"> Register Here !</Link>
-              </p> */}
-              <Button type="primary" htmlType="submit">
-                Login
-              </Button>
-            </div>
-          </Form>
-        </div>
+    <div className="register">
+      <div className="regsiter-form">
+        <h1>Login</h1>
+        <Form layout="vertical" onFinish={handleSubmit}>
+          <Form.Item name="userId" label="User ID">
+            <Input required />
+          </Form.Item>
+          <Form.Item name="password" label="Password">
+            <Input required type="password" />
+          </Form.Item>
+          {/* <p className="test-creds">UserId: 123 | Password: 123</p> */}
+          <div className="d-flex justify-content-between">
+            <Button type="primary" htmlType="submit">
+              {isLoading ? <FaC size={20} className="loader" /> : "Login"}
+            </Button>
+          </div>
+        </Form>
       </div>
-    </>
+    </div>
   );
 };
 
